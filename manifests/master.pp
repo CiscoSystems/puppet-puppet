160d07e69eab67aa0522ca7af6f19f67b64d03f1
Gary Larizza <gary@puppetlabs.com>
2011-08-15 12:41:24 -0400

    Documentation for master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -3,14 +3,34 @@
 # This class installs and configures a Puppet master
 #
 # Parameters:
-# * modulepath
-# * storeconfigs
-# * dbadapter
-# * dbuser
-# * dbpassword
-# * dbserver
-# * dbsocket
-# * certname
+#   [*modulepath*]            - The modulepath configuration value used in
+#                               puppet.conf
+#   [*confdir*]               - The confdir configuration value in puppet.conf
+#   [*manifest*]              - The manifest configuration value in puppet.conf
+#   [*storeconfigs*]          - Boolean determining whether storeconfigs is
+#                               to be enabled.
+#   [*storeconfigs_dbadapter*] - The database adapter to use with storeconfigs
+#   [*storeconfigs_dbuser*]   - The database username used with storeconfigs
+#   [*storeconfigs_dbpassword*] - The database password used with storeconfigs
+#   [*storeconfigs_dbserver*]   - Fqdn of the storeconfigs database server
+#   [*storeconfigs_dbsocket*]   - The path to the mysql socket file
+#   [*install_mysql_pkgs*]      - Boolean determining whether mysql and related
+#                                 devel packages should be installed.
+#   [*certname*]              - The certname configuration value in puppet.conf
+#   [*autosign*]              - The autosign configuration value in puppet.conf
+#   [*dashboard_port*]          - The port on which puppet-dashboard should run
+#   [*puppet_passenger*]      - Boolean value to determine whether puppet is
+#                               to be run with Passenger
+#   [*puppet_site*]           - The VirtualHost value used in the apache vhost
+#                               configuration file when Passenger is enabled
+#   [*puppet_docroot*]        - The DocumentRoot value used in the apache vhost
+#                               configuration file when Passenger is enabled
+#   [*puppet_passenger_port*] - The port on which puppet is listening when
+#                               Passenger is enabled
+#   [*puppet_master_package*]   - The name of the puppet master package
+#   [*package_provider*]        - The provider used for package installation
+#   [*version*]               - The value of the ensure parameter for the
+#                               puppet master and agent packages
 #
 # Actions:
 #
