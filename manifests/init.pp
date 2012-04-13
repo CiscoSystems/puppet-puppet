3e78248151764c24ad44c736f7c0f132df1be166
Soren Hansen <sorhanse@cisco.com>
2012-04-13 21:35:24 +0200

    Import puppet module

---
+++ manifests/init.pp
@@ -0,0 +1,56 @@
+class puppet($run_master = false, $run_agent = false, $puppetmaster_address = "") {
+	package { puppet-common:
+		ensure => present
+	}
+
+	if ($run_master) {
+		package { puppetmaster:
+			ensure => present
+		}
+
+		File <| title == "/etc/puppet/puppet.conf" |> {
+			notify +> Exec["restart-puppetmaster"]
+		}
+
+		exec { "restart-puppetmaster":
+			command => "/usr/sbin/service puppetmaster restart",
+			require => Package[puppetmaster],
+			refreshonly => true
+		}
+	}
+
+	if ($run_agent) {
+		package { puppet:
+			ensure => present
+		}
+
+		file { "/etc/default/puppet":
+			content => template('puppet/defaults.erb'),
+			notify => Exec["restart-puppet"],
+		}
+
+		File <| title == "/etc/puppet/puppet.conf" |> {
+			notify +> Exec["restart-puppet"]
+		}
+
+		file { "/etc/init.d/puppet":
+			mode => 0755,
+			owner => root,
+			group => root,
+			content => template('puppet/init.erb'),
+			notify => Exec["restart-puppet"]
+		}
+
+		exec { "restart-puppet":
+			command => "/usr/sbin/service puppet restart",
+			require => Package[puppet],
+			refreshonly => true
+		}
+
+	}
+
+	file { "/etc/puppet/puppet.conf":
+		content => template('puppet/puppet.conf.erb'),
+		require => Package[puppet-common]
+	}
+}
