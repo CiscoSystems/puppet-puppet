da9b0e9a6ab85ba3a99ecabb2c4fe33b5fb7cc47
Pradeep Kilambi <pkilambi@cisco.com>
2013-06-11 10:21:21 -0400

    fix line spacing and moving params to a separate params.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,15 +9,16 @@ class puppet(
   $mysql_root_password = 'changeMe',
   $mysql_password = 'changeMe'
 ) {
+  include puppet::params
+  
+  package { $::puppet::params::puppet:
+    ensure => present
+  }
 
-	package { 'puppet-common':
-		ensure => present
-	}
-
-	if ($run_master) {
-		package { "puppetmaster-passenger":
-			ensure => present
-		}
+  if ($run_master) {
+    package { $::puppet::params::puppetmaster:
+      ensure => present
+    }
 
     # now fix the rails.log permissions issue
     file { 'railslog':
@@ -28,19 +29,19 @@ class puppet(
       group   => "puppet",
     }
 
-		# set up mysql server
-		class { 'mysql::server':
-			config_hash => {
-				'root_password' => $mysql_root_password,
-				'bind_address'  => '127.0.0.1'
-			}
-		}
+    # set up mysql server
+    class { 'mysql::server':
+      config_hash => {
+        'root_password' => $mysql_root_password,
+        'bind_address'  => '127.0.0.1'
+      }
+    }
 
-		mysql::db { puppet:
-			user         => puppet,
-			password     => $mysql_password,
-			host         => localhost,
-		}
+    mysql::db { puppet:
+      user         => puppet,
+      password     => $mysql_password,
+      host         => localhost,
+    }
 
     file { "/var/lib/puppet/reports":
       ensure => "directory",
@@ -48,68 +49,74 @@ class puppet(
       group  => "puppet"
     }
 
-		package { "ruby-activerecord":
-			ensure => present
-		}
+    package { $::puppet::params::activerecord:
+      ensure => present
+    }
 
-		package { "ruby-mysql":
-			ensure => present
-		}
+    package { "ruby-mysql":
+      ensure => present
+    }
 
-		File <| title == "/etc/puppet/puppet.conf" |> {
-			notify +> Exec["restart-puppetmaster"]
-		}
+    File <| title == "/etc/puppet/puppet.conf" |> {
+      notify +> Service[$::puppet::params::apache_service]
+    }
 
-		file { "/etc/puppet/autosign.conf":
-			ensure => present,
-			content   => template('puppet/autosign.conf.erb'),
-		}
+    file { "/etc/puppet/autosign.conf":
+      ensure => present,
+      content   => template('puppet/autosign.conf.erb'),
+    }
+                
+   service { "puppetmaster":
+     ensure => "running",
+     require => Package[ $::puppet::params::puppetmaster ],
+   }
+   
+   if !defined( Service[$::puppet::params::apache_service] ) {
+     service { $::puppet::params::apache_service:
+       ensure => "running",
+       notify => Service["puppetmaster"]
+     }
+   }
+  }
 
-		exec { "restart-puppetmaster":
-			command => "/usr/sbin/service apache2 restart",
-			require => Package["puppetmaster-passenger"],
-			refreshonly => true
-		}
-	}
+  if ($run_agent) {
+    package { puppet:
+      ensure => present
+    }
 
-	if ($run_agent) {
-		package { puppet:
-			ensure => present
-		}
+    file { "/etc/default/puppet":
+      content => template('puppet/defaults.erb'),
+      notify => Service["puppet"],
+    }
 
-		file { "/etc/default/puppet":
-			content => template('puppet/defaults.erb'),
-			notify => Exec["restart-puppet"],
-		}
+    File <| title == "/etc/puppet/puppet.conf" |> {
+      notify +> Service["puppet"]
+    }
 
-		File <| title == "/etc/puppet/puppet.conf" |> {
-			notify +> Exec["restart-puppet"]
-		}
+    file { "/etc/init.d/puppet":
+      mode => 0755,
+      owner => root,
+      group => root,
+      content => template('puppet/init.erb'),
+      notify => Service["puppet"]
+    }
 
-		file { "/etc/init.d/puppet":
-			mode => 0755,
-			owner => root,
-			group => root,
-			content => template('puppet/init.erb'),
-			notify => Exec["restart-puppet"]
-		}
+    service { "puppet":
+      ensure  => "running",
+      require => Package[puppet],
+      refreshonly => true
+    }
 
-		exec { "restart-puppet":
-			command => "/usr/sbin/service puppet restart",
-			require => Package[puppet],
-			refreshonly => true
-		}
+  }
 
-	}
-
-	file { "/etc/puppet/puppet.conf":
-		content => template('puppet/puppet.conf.erb'),
-		require => Package[puppet-common]
-	}
+  file { "/etc/puppet/puppet.conf":
+    content => template('puppet/puppet.conf.erb'),
+    require => Package[$::puppet::params::puppet]
+  }
 
   file { "/etc/cron.d/puppet_cleanup":
     content => template('puppet/puppet_cleanup.erb'),
-    require => Package[puppet-common],
+    require => Package[$::puppet::params::puppet],
     owner   => root,
     group   => root,
     mode    => 0644
