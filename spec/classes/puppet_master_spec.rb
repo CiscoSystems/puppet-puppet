2a663276bddcf28a93616451e83f5c4e5f1edb49
Pall Valmundsson <pall.valmundsson@gmail.com>
2013-10-25 12:09:21 +0000

    added RedHat spec tests for puppet::master

--- spec/classes/puppet_master_spec.rb
+++ spec/classes/puppet_master_spec.rb
@@ -126,4 +126,128 @@ describe 'puppet::master', :type => :class do
             should contain_anchor('puppet::master::end')
         }
     end
+
+    context 'on RedHat operatingsystems' do
+        let(:facts) do
+            {
+                :osfamily        => 'RedHat',
+                :operatingsystem => 'RedHat',
+                :operatingsystemrelease => '6',
+                :concat_basedir => '/nde',
+            }
+        end
+        let (:params) do
+            {
+                :version                => 'present',
+                :puppet_master_package  => 'puppetmaster',
+                :puppet_master_service  => 'puppetmaster',
+                :modulepath             => '/etc/puppet/modules',
+                :manifest               => '/etc/puppet/manifests/site.pp',
+                :autosign               => 'true',
+                :certname               => 'test.example.com',
+                :storeconfigs           => 'true',
+                :storeconfigs_dbserver  => 'test.example.com'
+
+            }
+        end
+        it {
+            should contain_user('puppet').with(
+                :ensure => 'present',
+                :uid    => nil,
+                :gid    => 'puppet'
+            )
+            should contain_group('puppet').with(
+                :ensure => 'present',
+                :gid    => nil
+            )
+            should contain_package(params[:puppet_master_package]).with(
+                :ensure => params[:version],
+            )
+            should_not contain_package('puppetmaster-common').with(
+                :ensure => params[:version]
+            )
+            should contain_service(params[:puppet_master_service]).with(
+                :ensure    => 'stopped',
+                :enable    => 'false',
+                :require   => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_file('/etc/puppet/puppet.conf').with(
+                :ensure  => 'file',
+                :require => 'File[/etc/puppet]',
+                :owner   => 'puppet',
+                :group   => 'puppet',
+                :notify  => 'Service[httpd]'
+            )
+            should contain_file('/etc/puppet').with(
+                :require => "Package[#{params[:puppet_master_package]}]",
+                :ensure => 'directory',
+                :owner  => 'puppet',
+                :group  => 'puppet',
+                :notify => "Service[httpd]"
+            )
+            should contain_file('/var/lib/puppet').with(
+                :ensure => 'directory',
+                :owner  => 'puppet',
+                :group  => 'puppet',
+                :notify => 'Service[httpd]'
+            )
+            should contain_class('puppet::storeconfigs').with(
+              :before => 'Anchor[puppet::master::end]'
+            )
+            should contain_class('puppet::passenger').with(
+              :before => 'Anchor[puppet::master::end]'
+            )
+            should contain_ini_setting('puppetmastermodulepath').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'modulepath',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => params[:modulepath],
+                :require => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_ini_setting('puppetmastermanifest').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'manifest',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => params[:manifest],
+                :require => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_ini_setting('puppetmasterautosign').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'autosign',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => params[:autosign],
+                :require => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_ini_setting('puppetmastercertname').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'certname',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => params[:certname],
+                :require => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_ini_setting('puppetmasterreports').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'reports',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => 'store',
+                :require => 'File[/etc/puppet/puppet.conf]'
+            )
+            should contain_ini_setting('puppetmasterpluginsync').with(
+                :ensure  => 'present',
+                :section => 'master',
+                :setting => 'pluginsync',
+                :path    => '/etc/puppet/puppet.conf',
+                :value   => 'true'
+            )
+            should contain_anchor('puppet::master::begin').with_before(
+              ['Class[Puppet::Passenger]', 'Class[Puppet::Storeconfigs]']
+            )
+            should contain_anchor('puppet::master::end')
+        }
+    end
 end
